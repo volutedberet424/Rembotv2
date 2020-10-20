6bd8047b018beba9b7c614fd1bb984a8b6ee1899
volutedberet424 <60577740+volutedberet424@users.noreply.github.com>
2020-10-20 22:26:46 +0200

    Update Main.py

--- Main.py
+++ Main.py
@@ -169,7 +169,7 @@ async def updateok(ctx):
 
 @client.command()
 async def randomszám(ctx, num1, num2):
-    outputnumber = random.randint(num1, num2) 
+    outputnumber = random.randint(min(num1), max(num2)) 
     await ctx.send(f"{outputnumber}")
 
 
