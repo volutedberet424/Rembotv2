c55ca373dd0ea4cc04cb3717a157b46ffa1454ba
volutedberet424 <60577740+volutedberet424@users.noreply.github.com>
2020-12-01 16:09:05 +0100

    Update Main.py

--- Main.py
+++ Main.py
@@ -660,11 +660,6 @@ async def blush(ctx):
     await ctx.send(embed=blembed)
 
 
-@client.event
-async def on_message(messagea):
-    print(messagea)
-
-
 
 @client.event
 async def on_command_error(error, ctx):
